fdb9b4be4b795b2dc760d1b7f89a7818e9a0d75c
Dorant Velija <dorantvelija@outlook.com>
2026-05-03 16:40:38 +0200

    random forest analysis, optimization, feature importance analysis.

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ from maps import *
 from cluster import *
 from tree import *
 
-
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 24)
 
 df = pd.read_csv('german.data', sep=r'\s+', header=None, names=headers)
-
 map_codes(df)
-cluster_plot(df)
-decision_tree_plot(df)
+df_numerical = pd.read_csv('german.data-numeric', sep=r'\s+', header=None)
+
+# cluster_plot(df)
+# decision_tree_plot(df)
+
+
+forest_comparison(0.20, 0.40, 0.05, df_numerical)
+forest_comparison(0.25, 0.35, 0.01225, df_numerical)
+
+forest_feature_importance(df_numerical, 0.35, 2**8)
+forest_feature_importance(df_numerical, 0.32, 2**8)
+forest_feature_importance(df_numerical, 0.25, 2**7)
--- tree.py
+++ tree.py
@@ -1,13 +1,71 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
 
 def decision_tree_plot(df):
-    X = df[['Credit amount', 'Duration', 'Age', 'Installment rate in % income']]
-    y = df['Risk']
+        X = df[['Credit amount', 'Duration', 'Age', 'Installment rate in % income']]
+        y = df['Risk']
 
-    clf = DecisionTreeClassifier()
-    clf.fit(X, y)
+        clf = DecisionTreeClassifier()
+        clf.fit(X, y)
 
-    plt.figure(figsize = (100,100))
-    plot_tree(clf, feature_names=X.columns, class_names=['No Risk', 'Risk'], filled=True)
+        plt.figure(figsize = (100,100))
+        plot_tree(clf, feature_names=X.columns, class_names=['No Risk', 'Risk'], filled=True)
+        plt.show()
+
+def random_forest(df, size, random_state):
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = size, random_state = random_state)
+        clf = RandomForestClassifier(n_estimators = 100, random_state = random_state)
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+        score = clf.score(X_test, y_test)
+
+        importances = clf.feature_importances_
+
+        cm = confusion_matrix(y_test, y_pred)
+        cost_matrix = (cm[0,1] * 1 + cm[1,0] *5)
+
+        print(f'Score {round(score, 4) * 100}%, Misclassifications {cost_matrix}, Good when bad weighted: {cm[1,0] * 5} Bad when good weighted: {cm[0,1] * 1}')
+
+        # score, cost matrix, good when bad, bad when good, importances
+        return score, cost_matrix, {cm[1,0] * 5}, {cm[0,1] * 1}, importances
+
+def forest_feature_importance(df, size, random_state):
+    score,cost_matrix,neg_weight,pos_weight,importances = random_forest(df, size, random_state)
+    attributes = list(range(len(importances)))
+
+    bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+    plt.bar(attributes, importances, color=bar_colors, edgecolor='g', align='center')
+    plt.xlabel('Feature number')
+    plt.ylabel('Importance')
+    plt.title(f'Feature importance\nScore {round(score, 2) * 100}% Test {size} Random States 2^{round(np.log2(random_state))}')
+    plt.show()
+
+
+def forest_comparison(min_size, max_size, change_by, df):
+    plt.figure(figsize=(10, 6))
+    while min_size <= max_size:
+        forrest_maps = []
+        for i in range(16):
+            forrest_maps.append(random_forest(df, min_size, 2 ** i))
+
+        forest_scores = [item[0] for item in forrest_maps]
+        forrest_cost = [item[1] for item in forrest_maps]
+        forrest_iterations = list(range(len(forrest_maps)))
+
+        plt.plot(forrest_iterations, forest_scores, alpha=0.6, label=f'Test size:{round(min_size * 100)}%\nMean:{round(np.mean(forest_scores) * 100, 2)}±{round(np.std(forest_scores ) * 100, 2)}\nCV:{round((np.std(forrest_cost) / np.mean(forrest_cost)) * 100, 2)}%')
+
+        min_size = min_size + change_by
+
+    plt.xlabel('2^n random state')
+    plt.ylabel('Score')
+    plt.title(f'Score vs Iterations')
+    plt.legend(fontsize='x-small', loc='lower right')
     plt.show()
